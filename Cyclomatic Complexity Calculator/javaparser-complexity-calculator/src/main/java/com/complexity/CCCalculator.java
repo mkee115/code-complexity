c3edcd5d78558410fabf0ef0f6ac5acb90f655dc
Bailey Gibson <baileygibo@hotmail.com>
2026-04-15 13:37:50 +1200

    Make it so it accepts file name or folder as input

--- Cyclomatic Complexity Calculator/javaparser-complexity-calculator/src/main/java/com/complexity/CCCalculator.java
+++ Cyclomatic Complexity Calculator/javaparser-complexity-calculator/src/main/java/com/complexity/CCCalculator.java
@@ -1,10 +1,14 @@
 package com.complexity;
 
+import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CCCalculator
 {
@@ -16,15 +20,50 @@ public class CCCalculator
         return counter[0];
     }
 
+    public static List<File> findJavaFiles(File dir)
+    {
+        List<File> results = new ArrayList<>();
+        for (File f : dir.listFiles())
+        {
+            if (f.isDirectory())
+                results.addAll(findJavaFiles(f));
+            else if (f.getName().endsWith(".java"))
+                results.add(f);
+        }
+        return results;
+    }
+
+    public static void processFile(File file) throws Exception
+    {
+        CompilationUnit cu = StaticJavaParser.parse(file);
+        List<MethodDeclaration> methods = cu.findAll(MethodDeclaration.class);
+        if (methods.isEmpty()) return;
+
+        System.out.println(file.getName());
+        methods.forEach(method -> {
+            int cc = computeCC(method);
+            System.out.println("\t" + method.getNameAsString() + ": " + cc);
+        });
+    }
+
     public static void main(String[] args) throws Exception
     {
-        System.out.print("File name: ");
-        String fileName = System.console().readLine();
-        CompilationUnit cu = StaticJavaParser.parse(new File(fileName));
+        StaticJavaParser.getParserConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_17);
 
-        cu.findAll(MethodDeclaration.class).forEach(method -> {
-            int cc = computeCC(method);
-            System.out.println(method.getNameAsString() + ": " + cc);
-        });
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("File or folder: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        File target = new File(input);
+
+        if (target.isDirectory())
+        {
+            for (File f : findJavaFiles(target))
+                processFile(f);
+        }
+        else
+        {
+            processFile(target);
+        }
     }
 }
